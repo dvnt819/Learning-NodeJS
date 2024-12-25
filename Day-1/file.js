c3af79dec64373d086cc2e9955e932232ebb1617
Dhvanit <dhvanit03@gmail.com>
2024-12-25 11:27:28 +0530

    Initial commit of learning NodeJS Day-1

---
+++ Day-1/file.js
@@ -0,0 +1,29 @@
+const fs=require("fs");
+
+// fs.writeFileSync('./test.txt','hello world There');
+
+// fs.writeFile('./test.txt','hello world There wow',(err)=>{});
+
+// const result=fs.readFile("./contacts.txt","utf-8");
+
+// fs.readFile("./contacts.txt","utf-8",(err,result)=>{
+//     if(err){
+//         console.log("Error",err);
+//     }else{
+//         console.log(result)
+//     }
+// });
+
+// fs.appendFileSync("./test.txt", new Date().getDate().toLocaleString());
+
+// fs.cpSync('./test.txt','./copy.txt');
+
+// fs.unlinkSync('./copy.txt');
+
+// console.log(fs.statSync('./test.txt').isFile());
+
+// console.log(fs.statSync("./test.txt"));
+
+// fs.mkdirSync("my-docs/a/b/c",{recursive:true});
+
+// fs.rm("my-docss",{recursive:true});
---
+++ Day-1/nice.js
@@ -0,0 +1,4 @@
+const math=require('./hello')
+console.log("This file is nice");
+
+console.log(math.add(9,11))
