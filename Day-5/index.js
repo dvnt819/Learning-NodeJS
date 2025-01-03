473431f771467b20049beae37b04f0a7edfccf1c
Dhvanit <dhvanit03@gmail.com>
2025-01-03 22:34:57 +0530

    Updated Day-5

--- Day-5/index.js
+++ Day-5/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const fs = require("fs")
 const users = require('./MOCK_DATA.json');
 const app= express();
 const PORT = 8000;
+
+app.use(express.urlencoded({extended:false}));
 
 app.get("/users",(req,res)=>{
     const html = `
@@ -23,9 +26,11 @@ app.get("/api/users/:id",(req,res)=>{
 })
 
 app.post("/api/users",(req,res)=>{
-
-
-    return res.json({status:"pending"});
+    const body = req.body;
+    users.push({...body,id:users.length+1});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        return res.json({status:"success",id: users.length});
+    })
 })
 
 app.patch("api/users/:id",(req,res)=>{
